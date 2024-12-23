f8603c83a144148307b27d4cb7de760573b1d0fd
jljl1337 <43352209+jljl1337@users.noreply.github.com>
2024-12-22 21:39:59 -0700

    feat: add build step in action (#6)
    * update file names in python script
    
    * fix substring extraction
    
    * fix variable references in build workflow
    
    * add build and push step
    
    * fix build argument
    
    * change to run every day

--- tags.py
+++ tags.py
@@ -10,26 +10,22 @@ with urlopen(url) as response:
     try:
         jsonbody = json.load(response)
     except json.JSONDecodeError:
+        print('Failed to get JSON from Docker Hub')
         exit(1)
 
 tags = jsonbody['results']
 
 # Filter by last_updated that is with the past week
-one_week_ago = datetime.datetime.now() - datetime.timedelta(days=7)
-one_week_ago = one_week_ago.replace(tzinfo=datetime.timezone.utc).isoformat()
+one_day_ago = datetime.datetime.now() - datetime.timedelta(days=1)
+one_day_ago = one_day_ago.replace(tzinfo=datetime.timezone.utc).isoformat()
 
-filtered = [tag for tag in tags if tag['last_updated'] > one_week_ago]
+filtered = [tag for tag in tags if tag['last_updated'] > one_day_ago]
 filtered = [
     tag for tag in filtered if len([
         True for image in tag['images'] if
         image['architecture'] == 'amd64' and image['os'] == 'linux'
     ]) > 0
 ]
-
-print(f'{len(filtered)} tags found')
-for tag in filtered:
-    print(tag['name'])
-    # print(tag['last_updated'])
 
 rhel = []
 alpine = []
@@ -43,22 +39,18 @@ for tag in filtered:
     else:
         debian.append(tag)
 
+print('Filtered tags:')
 print(f'RHEL: {len(rhel)}')
 print(f'Alpine: {len(alpine)}')
 print(f'Debian: {len(debian)}')
 
-for tag in alpine:
-    print(tag['name'])
-
-rhel = [f'rhel,{tag["name"]}' for tag in rhel]
-alpine = [f'alpine,{tag["name"]}' for tag in alpine]
-debian = [f'debian,{tag["name"]}' for tag in debian]
+rhel = [f'rhel.Dockerfile,{tag["name"]}' for tag in rhel]
+alpine = [f'alpine.Dockerfile,{tag["name"]}' for tag in alpine]
+debian = [f'debian.Dockerfile,{tag["name"]}' for tag in debian]
 
 all_tags = rhel + alpine + debian
 
 all_tags_str = json.dumps({'tags': all_tags}, separators=(',', ':'))
-
-print(all_tags_str)
 
 with open(os.environ["GITHUB_OUTPUT"], "a") as f:
     f.write(f'matrix={all_tags_str}\n')
