3488e2761e0e4a25771b6fd76915cd4f536bb364
jljl1337 <lckjack123@gmail.com>
2024-12-22 20:54:33 -0700

    update file names in python script

--- tags.py
+++ tags.py
@@ -10,6 +10,7 @@ with urlopen(url) as response:
     try:
         jsonbody = json.load(response)
     except json.JSONDecodeError:
+        print('Failed to get JSON from Docker Hub')
         exit(1)
 
 tags = jsonbody['results']
@@ -26,11 +27,6 @@ filtered = [
     ]) > 0
 ]
 
-print(f'{len(filtered)} tags found')
-for tag in filtered:
-    print(tag['name'])
-    # print(tag['last_updated'])
-
 rhel = []
 alpine = []
 debian = []
@@ -43,22 +39,18 @@ for tag in filtered:
     else:
         debian.append(tag)
 
+print('Filtered tags:')
 print(f'RHEL: {len(rhel)}')
 print(f'Alpine: {len(alpine)}')
 print(f'Debian: {len(debian)}')
 
-for tag in alpine:
-    print(tag['name'])
-
-rhel = [f'rhel,{tag["name"]}' for tag in rhel]
-alpine = [f'alpine,{tag["name"]}' for tag in alpine]
-debian = [f'debian,{tag["name"]}' for tag in debian]
+rhel = [f'rhel.Dockerfile,{tag["name"]}' for tag in rhel]
+alpine = [f'alpine.Dockerfile,{tag["name"]}' for tag in alpine]
+debian = [f'debian.Dockerfile,{tag["name"]}' for tag in debian]
 
 all_tags = rhel + alpine + debian
 
 all_tags_str = json.dumps({'tags': all_tags}, separators=(',', ':'))
-
-print(all_tags_str)
 
 with open(os.environ["GITHUB_OUTPUT"], "a") as f:
     f.write(f'matrix={all_tags_str}\n')
